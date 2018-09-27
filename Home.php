7a6c6c344c2746721412ec38c45974e2a0378c86
Asaelem <dylans07@yahoo.com>
2018-09-26 22:50:20 -0700

    Home page + blue_ribbon CSS
    I added the blue ribbon CSS, but it is no where near complete

--- Home.php
+++ Home.php
@@ -11,30 +11,34 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Home Page</title>
-    <link rel="stylesheet" type="text/css" href="CSS/Nav_bar.css">
-    <script src="JS/nav_bar.js"></script>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home page</title>
+    <link href="BootStrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="CSS/blue_ribbon.css" rel="stylesheet">
+
 </head>
 
 <header>
-
-    <nav>
-
-        <ul>
-            <li><a href="home.php">Home</a> </li>
-            <li><a href="rental.php">Rentals</a> </li>
-            <li><a href="commission.php">Commission</a> </li>
-            <li><a href="contact.php">Contact Us</a> </li>
-
-        </ul>
-
-    </nav>
-
+    <div >
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark" id="navbar">
+            <ul class="navbar-nav" id="nav-ul">
+                <li class="nav-item active"><a class="nav-link" id="nav-text" href="Home.php">Home</a> </li>
+                <li class="nav-item active"><a class="nav-link" href="rental.php">Rental</a> </li>
+                <li class="nav-item active"><a class="nav-link" href="Commission.php">Commission</a> </li>
+                <li class="nav-item active"><a class="nav-link" href="Contact.php">Contact</a> </li>
+            </ul>
+        </nav>
+    </div>
 </header>
 
 <body>
 
 
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="BootStrap/js/bootstrap.min.js"></script>
 </body>
 
 <footer>
