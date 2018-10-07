bdf013c37cd5c956a3ad5c50640760e5d2849d89
Grunkzilla <asplund.alex@gmail.com>
2018-10-06 23:52:48 -0700

    Added footer

--- Home.php
+++ Home.php
@@ -20,6 +20,8 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="CSS/custom.css">
+
 
 </head>
 
@@ -82,8 +84,14 @@
 
 </body>
 
-<footer>
+<footer class="modal-footer">
 
+    <p>(509)000-0000</p>
+    <p>Insert Address</p>
+    <div id="social_icon">
+        <img src="images/fb_icon.png" alt="Facebook" href="https://www.facebook.com/BlueRibbonHWs/">
+        <img src="images/twit_icon.png" alt="Twiter">
+    </div>
 
 </footer>
 
