9ed668ce683ce46fbe2911c6730f9fb8881dc22b
Grunkzilla <asplund.alex@gmail.com>
2018-12-26 22:39:29 -0800

    bro messed up

--- admin.php
+++ admin.php
@@ -1,3 +1,29 @@
+<?php
+
+session_start();
+
+$error = '';
+
+if(isset($_POST['submit'])){
+
+if (empty($_POST['username']) || empty($_POST['password'])) {
+    $error = "Username or Password is invalid";
+}
+else{
+    $username='username';
+    $password='password';
+    if ($username == 'blueribbonhw' && $password == 'adminpassword'){
+        echo 'login success';
+    }
+    else{
+        $error='incorrect user or password';
+    }
+}
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
