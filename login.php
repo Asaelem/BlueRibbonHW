56488443c40f3ab035b37beff4ca6fc187529487
Grunkzilla <asplund.alex@gmail.com>
2018-12-27 22:02:36 -0800

    more login work

--- login.php
+++ login.php
@@ -1,24 +1,27 @@
 <?php
 
-session_start();
+//session_start();
 
 $error = '';
 
 if(isset($_POST['submit'])){
 
-    if (empty($_POST['username']) || empty($_POST['password'])) {
+    if (empty($_POST['username_input']) || empty($_POST['password_input'])) {
         $error = "Username or Password is invalid";
     }
     else{
-        $username='username';
-        $password='password';
+        $username='username_input';
+        $password='password_input';
         if ($username == 'blueribbonhw' && $password == 'adminpassword'){
-            echo 'login success';
+            header("location: http://localhost:63342/BlueRibbon/BlueRibbonHW/admin_panel.php?_ijt=evn1d7l7gttclqqvhsr6s2vf6q");
         }
         else{
             $error='incorrect user or password';
+            echo "$error";
         }
     }
 }
+
+
 
 ?>
